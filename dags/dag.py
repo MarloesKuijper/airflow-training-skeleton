92fa1f95a169f2fe1b5bcdbb9d2811d808ad0d43
Marloes Kuijper <mkuijper@bol.com>
2019-02-25 12:13:42 +0100

    create pipeline with print date and sleep

--- dags/dag.py
+++ dags/dag.py
@@ -1,6 +1,7 @@
 import airflow
 from airflow import DAG
 from airflow.operators.bash_operator import BashOperator
+from airflow.operators.dummy_operator import DummyOperator
 
 dag = DAG(
     dag_id="hello_airflow",
@@ -10,6 +11,17 @@ dag = DAG(
     },
 )
 
-BashOperator(
+print_exec_date = BashOperator(
     task_id="print_exec_date", bash_command="echo {{ execution_date }}", dag=dag
 )
+
+dummy_end = DummyOperator(
+    task_id="end", bash_command="", dag=dag
+)
+
+sleeping = [BashOperator(
+    task_id="sleeping_" + str(i), bash_command="sleep " + str(i), dag=dag
+) for i in [1, 5, 10]]
+
+print_exec_date >> sleeping >> dummy_end
+
